9f3b3c61a5c16913638ea7d2ef2736db6cde2eaa
Katie Mcgowan <andrea.woeger@gmx.net>
2021-08-27 15:43:27 +0200

    Add search input field

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,31 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import styles from '../../styles/Home.module.css';
 import Map from '../components/Map';
 
 const DEFAULT_CENTER = [38.907132, -77.036546];
 
 export default function Home() {
+  const [search, setSearch] = useState('');
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <h1>Find every country's national dish</h1>
+      <label aria-label="Search bar">
+        <input
+          name="Search"
+          placeholder="Search for continents"
+          value={search}
+          onChange={(event) => {
+            setSearch(event.currentTarget.value);
+          }}
+        />
+      </label>
 
       <Map className={styles.homeMap} center={DEFAULT_CENTER} zoom={12}>
         {({ TileLayer, Marker, Popup }) => (
