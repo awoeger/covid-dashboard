f35a10d4a78c84263b674c3ecd775d9dfd0a72ff
Katie Mcgowan <andrea.woeger@gmx.net>
2021-08-29 11:50:57 +0200

    Connect database.js to postgres

--- database.js
+++ database.js
@@ -2,9 +2,29 @@ import camelcaseKeys from 'camelcase-keys';
 import dotenvsafe from 'dotenv-safe';
 import postgres from 'postgres';
 
-// Connect to database
-const sql = postgres();
-
 // Read the PostgreSQL secret connection information
 // (host, database, username, password) from the .env file
-dotenvSafe.config();
+dotenvsafe.config();
+
+// Connect only once to the database
+// https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
+export function connectOneTimeToDatabase() {
+  let sql;
+
+  if (process.env.NODE_ENV === 'production') {
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.__postgresSqlClient) {
+      globalThis.__postgresSqlClient = postgres();
+    }
+    sql = globalThis.__postgresSqlClient;
+  }
+
+  return sql;
+}
+
+// Connect to PostgreSQL
+const sql = connectOneTimeToDatabase();
